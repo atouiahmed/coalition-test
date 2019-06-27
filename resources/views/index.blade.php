1e5943e1baef08ef484d7ebad55992306c29c584
Atoui Ahmed <ahmeddeveloper2013@gmail.com>
2019-06-27 14:35:06 +0100

    add modal for create new product

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,7 +17,7 @@
 
     <div class="row m-5">
         <div class="pull-left m-4">
-            <button class="btn btn-primary">Add</button>
+            <button class="btn btn-primary" data-toggle="modal" data-target="#createModal">Add</button>
             <button class="btn btn-danger">Delete</button>
         </div>
         <table class="table">
@@ -43,7 +43,44 @@
             </tr>
             </tbody>
         </table>
+        <div class="pull-right">
+            <p class="font-weight-bold">Total: 200</p>
+        </div>
+    </div>
+</div>
+<div id="createModal" class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Create Product</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-group">
+                        <label for="InputProductName">Product Name</label>
+                        <input required type="text" class="form-control" id="InputProductName" placeholder="Enter Product Name">
+                    </div>
+                    <div class="form-group">
+                        <label for="InputQuantity">Product Quantity</label>
+                        <input required type="number" class="form-control" id="InputQuantity"  placeholder="Enter Product Quantity">
+                    </div>
+                    <div class="form-group">
+                        <label for="InputPrice">Product Price</label>
+                        <input required type="number" class="form-control" id="InputPrice"  placeholder="Enter Product Price">
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
     </div>
 </div>
 </body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script src="js/script.js"></script>
 </html>
